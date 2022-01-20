33a9da18cb06b1470a96c01ca2e8f86a429e19b2
Ned Wilbur <Ned612@Gmail.com>
2022-01-19 21:22:59 -0500

    Added some planned methods

---
+++ Jumpcloud_APITest/Utils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jumpcloud_APITest
+{
+    internal static class Utils
+    {
+        internal static void StartService()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void StopService()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void GenerateHash(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void GetHash(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void GetStats()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
